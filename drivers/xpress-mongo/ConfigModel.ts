f563f224e7c1718b6626b4756c353d843eb4d064
trapcodeio <trapcode.io@gmail.com>
2021-10-19 18:38:57 +0100

    Add Indexs

--- drivers/xpress-mongo/ConfigModel.ts
+++ drivers/xpress-mongo/ConfigModel.ts
@@ -9,5 +9,12 @@ class ConfigModel extends XMongoModel {}
  */
 UseCollection(ConfigModel, "db-configs");
 
+Promise.all([
+    ConfigModel.native().createIndex({ key: 1 }),
+    ConfigModel.native().createIndex({ group: 1 }),
+    ConfigModel.native().createIndex({ group: 1, key: 1 }),
+    ConfigModel.native().createIndex({ autoload: 1 })
+]).catch(console.log);
+
 // Export Model as Default
 export default ConfigModel;
