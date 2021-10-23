c2263cac5909692feb9eaa9d03879421605d0c41
trapcodeio <trapcode.io@gmail.com>
2021-10-23 18:44:48 +0100

    Add reset command

--- drivers/xpress-mongo/DbConfigClass.ts
+++ drivers/xpress-mongo/DbConfigClass.ts
@@ -11,7 +11,7 @@ class DbConfigClass extends DbConfig {
     }
 
     /**
-     * Add Config Option
+     * Add Config Option.
      * @param data
      */
     static async addConfig(data: DbDataArray) {
